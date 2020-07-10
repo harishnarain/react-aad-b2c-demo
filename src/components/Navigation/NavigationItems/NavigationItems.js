b4768eb1c5b69f13012351579443a7b12f537495
Harish Narain <harishnarain@gmail.com>
2020-07-10 11:37:23 +1000

    Fixed formatting on ToolbarComponent and replaced Logout, Admin buttons on NavigationItems with an AccountCircle

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -1,41 +1,84 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import { makeStyles } from '@material-ui/core/styles'
+import React from "react";
+import { Link } from "react-router-dom";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
+import { IconButton } from "@material-ui/core";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
+import Fade from "@material-ui/core/Fade";
 
-import Aux from '../../../hoc/Aux/Aux';
+import Aux from "../../../hoc/Aux/Aux";
+import Logout from "../../Auth/Logout/Logout";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
-const NavigationItems = props => {
-    
-    const classes = useStyles();
+const NavigationItems = (props) => {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-    return(
-        <Aux>
-            <Typography variant="h6" className={classes.title}>
-                Azure AD B2C React Demo
-            </Typography>
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-            {props.isAuthenticated
-                ? <Button color="inherit">Admin</Button>
-                : null}
-            {!props.isAuthenticated
-                ? <Button color="inherit" component={Link} to="/auth">Sign in</Button>
-                : <Button color="inherit" component={Link} to="/logout">Logout</Button>}
-        </Aux>
-    );
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Aux>
+      <Typography variant="h6" className={classes.title}>
+        Azure AD B2C React Demo
+      </Typography>
+
+      {props.isAuthenticated ? (
+        <div>
+          <IconButton
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            <MenuItem onClick={handleClose}>Admin Console</MenuItem>
+            <MenuItem component={Link} to="/logout">
+              Logout
+            </MenuItem>
+          </Menu>
+        </div>
+      ) : null}
+      {!props.isAuthenticated ? (
+        <Button color="inherit" component={Link} to="/auth">
+          Sign in
+        </Button>
+      ) : null}
+    </Aux>
+  );
 };
 
 export default NavigationItems;
